85150b1fc42aab88c6dd603586b46844df6e5027
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:03:03 +1300

    Converted to PSR-2

--- code/Opacity.php
+++ code/Opacity.php
@@ -1,15 +1,16 @@
 <?php
-class Opacity extends StyleObject {
-  /**
+class Opacity extends StyleObject
+{
+    /**
    * FIELDS
    */
 
-  private static $db = array (
+  private static $db = array(
     'Name' => 'Text',
     'Value' => 'Float'
   );
 
-  private static $defaults = array (
+    private static $defaults = array(
     'Value' => 0
   );
 
@@ -17,24 +18,24 @@ class Opacity extends StyleObject {
    * DEFAULT RECORDS
    */
 
-  private static $default_records = array (
-    array (
+  private static $default_records = array(
+    array(
       'Name' => 'Transparent',
       'Value' => 0
     ),
-    array (
+    array(
       'Name' => 'Near Transparent',
       'Value' => 0.25
     ),
-    array (
+    array(
       'Name' => 'Moderate Transparent',
       'Value' => 0.5
     ),
-    array (
+    array(
       'Name' => 'Near Opaque',
       'Value' => 0.75
     ),
-    array (
+    array(
       'Name' => 'Opaque',
       'Value' => 1
     ),
@@ -46,7 +47,7 @@ class Opacity extends StyleObject {
 
   private static $default_sort='Value ASC';
 
-  private static $summary_fields = array (
+    private static $summary_fields = array(
     'Name' => 'Name',
     'Value' => 'Value',
     'CMSPreview' => 'Preview'
@@ -56,8 +57,9 @@ class Opacity extends StyleObject {
    * CMS FIELDS
    */
 
-  public function getCMSFields() {
-    $fields = parent::getCMSFields();
+  public function getCMSFields()
+  {
+      $fields = parent::getCMSFields();
     
     /**
      * MAIN TAB
@@ -65,23 +67,24 @@ class Opacity extends StyleObject {
 
     $tab = 'Root.Main';
 
-    $field = new TextField('Name');
-    $fields->addFieldToTab($tab, $field);
+      $field = new TextField('Name');
+      $fields->addFieldToTab($tab, $field);
 
-    $field = new NumericField('Value');
-    $fields->addFieldToTab($tab, $field);
+      $field = new NumericField('Value');
+      $fields->addFieldToTab($tab, $field);
 
-    $html = ViewableData::renderWith('Opacity_CMS_Preview');
-    $field = new LiteralField('Preview', $html);
-    $fields->addFieldToTab($tab, $field);
+      $html = ViewableData::renderWith('Opacity_CMS_Preview');
+      $field = new LiteralField('Preview', $html);
+      $fields->addFieldToTab($tab, $field);
     
-    return $fields;
+      return $fields;
   }
 
-  public function getCMSPreview() {
-    $html = ViewableData::renderWith('Opacity_CMS_Preview');
-    $obj = HTMLText::create();
-    $obj->setValue($html);
-    return $obj;
-  }
+    public function getCMSPreview()
+    {
+        $html = ViewableData::renderWith('Opacity_CMS_Preview');
+        $obj = HTMLText::create();
+        $obj->setValue($html);
+        return $obj;
+    }
 }
--- code/Opacity_SiteConfigExtension.php
+++ code/Opacity_SiteConfigExtension.php
@@ -1,21 +1,23 @@
 <?php
-class Opacity_SiteConfigExtension extends DataExtension {
-  /**
+class Opacity_SiteConfigExtension extends DataExtension
+{
+    /**
    *  CMS FIELDS
    */
 
-  public function updateCMSFields(FieldList $fields) {    
-    /**
+  public function updateCMSFields(FieldList $fields)
+  {
+      /**
      *  APPEARANCE TAB
      */
 
     $tab = 'Root.Appearance.Opacities';
     
-    $conf=GridFieldConfig_RelationEditor::create(10);
-    $conf->removeComponentsByType('GridFieldPaginator');
-    $conf->removeComponentsByType('GridFieldPageCount');
-    $data = DataObject::get('Opacity');
-    $field = new GridField('Opactiy', 'Opacities', $data, $conf);
-    $fields->addFieldToTab($tab, $field);
+      $conf=GridFieldConfig_RelationEditor::create(10);
+      $conf->removeComponentsByType('GridFieldPaginator');
+      $conf->removeComponentsByType('GridFieldPageCount');
+      $data = DataObject::get('Opacity');
+      $field = new GridField('Opactiy', 'Opacities', $data, $conf);
+      $fields->addFieldToTab($tab, $field);
   }
 }
